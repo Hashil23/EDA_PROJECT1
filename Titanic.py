5fab7fbb1fb9f32b81a6f481dbf07abb3c68a297
Mohammed Hashil <hashilashi1007@gmail.com>
2025-12-28 13:52:53 +0530

    Dataset inspected

--- Titanic.py
+++ Titanic.py
@@ -2,4 +2,6 @@ import pandas as pd
 #load dataset
 data="https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
 df=pd.read_csv(data)
-print(df.head())
+
+#inspect data
+print(df.describe())
